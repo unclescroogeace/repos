6e52ef779416fbba25858d1e4f68188d29d99b79
Krasimir Kostadinov <33804692+unclescroogeace@users.noreply.github.com>
2021-09-01 15:12:25 +0300

    Initial commit

---
+++ Interface Implementation/Interface Implementation/ICustomList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Implementation
+{
+    public interface ICustomList<T>
+    {
+        public int Count { get; }
+        public void Add(T item);
+
+        /// <summary>
+        /// Removes the first occurrence of an item in the list.
+        /// </summary>
+        /// <param name="item">The item to remove</param>
+        /// <returns>false if the item was not found true otherwise</returns>
+        bool Remove(T item);
+        void Insert(int index, T item);
+        T this[int index] { get; set; }
+    }
+}
---
+++ Interface Implementation/Interface Implementation/Program.cs
@@ -0,0 +1,112 @@
+﻿using Interface_Implementation;
+using System.Collections;
+
+namespace CustomListNamespace
+{
+    public class Basket<T> : ICustomList<T>, IEnumerable<T>
+    {
+        List<T> list = new List<T>();
+        public T this[int index] { get => list[index]; set => list[index] = value; }
+
+        public int Count { get => list.Count; }
+
+        public void Add(T item)
+        {
+            list.Add(item);
+        }
+
+        public void Insert(int index, T item)
+        {
+            list.Insert(index, item);
+        }
+
+        public bool Remove(T item)
+        {
+            if (list.Remove(item))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T item in list)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    class Fruit{
+            string Name { get; set; }
+            string Color { get; set; }
+            int Sweetness { get; set; }
+
+            public Fruit() { }
+
+            public Fruit(string Name, string Color, int Sweetness)
+            {
+            this.Name = Name;
+            this.Color = Color;
+            this.Sweetness = Sweetness;
+            }
+
+            public void print()
+            {
+            Console.WriteLine(Name + " " + Color + " " + Sweetness);
+            }
+        }
+
+    class TestClass
+    {
+        static void Main(string[] args)
+        {
+            Basket<Fruit> basket = new Basket<Fruit>();
+            Fruit redApple = new Fruit("Apple", "Red", 8);
+            Fruit greenApple = new Fruit("Apple", "Green", 3);
+            Fruit yellowApple = new Fruit("Apple", "Yellow", 5);
+            Fruit orange = new Fruit("Orange", "Orange", 3);
+            Fruit melon = new Fruit("Melon", "Yellow", 9);
+
+            basket.Add(redApple);
+            basket.Add(greenApple);
+            basket.Add(yellowApple);
+            basket.Add(orange);
+
+            foreach(Fruit fruit in basket)
+            {
+                fruit.print();
+            }
+
+            basket.Remove(yellowApple);
+
+            Console.WriteLine();
+
+            foreach (Fruit fruit in basket)
+            {
+                fruit.print();
+            }
+
+            basket.Insert(2, melon);
+
+            Console.WriteLine();
+
+            foreach (Fruit fruit in basket)
+            {
+                fruit.print();
+            }
+
+        }
+    }
+
+
+}
