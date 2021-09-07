ef654666ce2cbe979b41024d79c427e80c900a6b
Krasimir Kostadinov <33804692+unclescroogeace@users.noreply.github.com>
2021-09-07 11:43:59 +0300

    Code refactoring + Method summary added

--- Integer Romanization/Integer Romanization/Program.cs
+++ Integer Romanization/Integer Romanization/Program.cs
@@ -4,63 +4,68 @@ namespace Integer_Romanization
 {
     class Program
     {
-        private static string Romanize(int integer)
+        /// <summary>
+        /// This method converts a integer to Roman numeral
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        private static string ToRomanNumeral(int number)
         {
-            if (integer < 1)
+            if (number < 1)
             {
                 return string.Empty;
             }
-            if (integer >= 1000)
+            if (number >= 1000)
             {
-                return "M" + Romanize(integer - 1000);
+                return "M" + ToRomanNumeral(number - 1000);
             }
-            else if (integer >= 900)
+            else if (number >= 900)
             {
-                return "CM" + Romanize(integer - 900);
+                return "CM" + ToRomanNumeral(number - 900);
             }
-            else if (integer >= 500)
+            else if (number >= 500)
             {
-                return "D" + Romanize(integer - 500);
+                return "D" + ToRomanNumeral(number - 500);
             }
-            else if (integer >= 400)
+            else if (number >= 400)
             {
-                return "CD" + Romanize(integer - 400);
+                return "CD" + ToRomanNumeral(number - 400);
             }
-            else if (integer >= 100)
+            else if (number >= 100)
             {
-                return "C" + Romanize(integer - 100);
+                return "C" + ToRomanNumeral(number - 100);
             }
-            else if (integer >= 90)
+            else if (number >= 90)
             {
-                return "XC" + Romanize(integer - 90);
+                return "XC" + ToRomanNumeral(number - 90);
             }
-            else if (integer >= 50)
+            else if (number >= 50)
             {
-                return "L" + Romanize(integer - 50);
+                return "L" + ToRomanNumeral(number - 50);
             }
-            else if (integer >= 40)
+            else if (number >= 40)
             {
-                return "XL" + Romanize(integer - 40);
+                return "XL" + ToRomanNumeral(number - 40);
             }
-            else if (integer >= 10)
+            else if (number >= 10)
             {
-                return "X" + Romanize(integer - 10);
+                return "X" + ToRomanNumeral(number - 10);
             }
-            else if (integer >= 9)
+            else if (number >= 9)
             {
-                return "IX" + Romanize(integer - 9);
+                return "IX" + ToRomanNumeral(number - 9);
             }
-            else if (integer >= 5)
+            else if (number >= 5)
             {
-                return "V" + Romanize(integer - 5);
+                return "V" + ToRomanNumeral(number - 5);
             }
-            else if (integer >= 4)
+            else if (number >= 4)
             {
-                return "IV" + Romanize(integer - 4);
+                return "IV" + ToRomanNumeral(number - 4);
             }
-            else if (integer >= 1)
+            else if (number >= 1)
             {
-                return "I" + Romanize(integer - 1);
+                return "I" + ToRomanNumeral(number - 1);
             }
             throw new ArgumentOutOfRangeException("Something went wrong!");
         }
@@ -74,7 +79,7 @@ namespace Integer_Romanization
                 Console.Write("Enter number between 1 and 3999: ");
                 int.TryParse(Console.ReadLine(), out integer);
             } while (integer <= 0 || integer >= 4000);
-            Console.WriteLine($"{integer} in roman is: {Romanize(integer)}");
+            Console.WriteLine($"{integer} in roman is: {ToRomanNumeral(integer)}");
         }
     }
 }
