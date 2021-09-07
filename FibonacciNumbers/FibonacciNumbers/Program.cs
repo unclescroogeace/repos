d9e4225aa2e090466c02d40560854fd8b53f9825
Krasimir Kostadinov <33804692+unclescroogeace@users.noreply.github.com>
2021-09-07 15:39:30 +0300

    commit

--- FibonacciNumbers/FibonacciNumbers/Program.cs
+++ FibonacciNumbers/FibonacciNumbers/Program.cs
@@ -4,9 +4,54 @@ namespace FibonacciNumbers
 {
     class Program
     {
+        private static void FibonacciRecoursive(int a, int b, int num)
+        {
+            Console.Write(a + " ");
+            if (num > 1)
+            {
+                FibonacciRecoursive(b, a + b, --num);
+            }
+        }
+
+        private static int[] FibonacciSeries(int num)
+        {
+            int[] arr;
+            if (num < 2)
+            {
+                arr = new int[2];
+            }
+            else
+            {
+                arr = new int[num];
+            }
+            arr[0] = 0;
+            arr[1] = 1;
+            for (int i = 2; i < num; i++)
+            {
+                arr[i] = arr[i - 1] + arr[i - 2];
+            }
+            return arr;
+        }
+
+        private static void PrintFibonacciSeries(int[] arr, int num)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num;
+            do
+            {
+
+                Console.Write("Enter N: ");
+                int.TryParse(Console.ReadLine(), out num);
+            } while (num < 1);
+            FibonacciRecoursive(0, 1, num);
+            Console.WriteLine();
+            PrintFibonacciSeries(FibonacciSeries(num), num);
         }
     }
 }
