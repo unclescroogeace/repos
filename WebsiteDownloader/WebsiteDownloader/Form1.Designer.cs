1ccfef5dd6a77444f52c0a70f5fe2969d69f8ab7
Krasimir Kostadinov <33804692+unclescroogeace@users.noreply.github.com>
2021-09-08 17:26:17 +0300

    commit

---
+++ WebsiteDownloader/WebsiteDownloader/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace WebsiteDownloader
+{
+    partial class AsyncExample
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.normalExecute = new System.Windows.Forms.Button();
+            this.normalParallelExecute = new System.Windows.Forms.Button();
+            this.asyncExecute = new System.Windows.Forms.Button();
+            this.parallelAsyncExecute = new System.Windows.Forms.Button();
+            this.cancelation = new System.Windows.Forms.Button();
+            this.resultBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // normalExecute
+            // 
+            this.normalExecute.Location = new System.Drawing.Point(12, 21);
+            this.normalExecute.Name = "normalExecute";
+            this.normalExecute.Size = new System.Drawing.Size(651, 51);
+            this.normalExecute.TabIndex = 0;
+            this.normalExecute.Text = "Normal Execute";
+            this.normalExecute.UseVisualStyleBackColor = true;
+            this.normalExecute.Click += new System.EventHandler(this.normalExecute_Click);
+            // 
+            // normalParallelExecute
+            // 
+            this.normalParallelExecute.Location = new System.Drawing.Point(12, 78);
+            this.normalParallelExecute.Name = "normalParallelExecute";
+            this.normalParallelExecute.Size = new System.Drawing.Size(651, 51);
+            this.normalParallelExecute.TabIndex = 1;
+            this.normalParallelExecute.Text = "Normal Parallel Execute";
+            this.normalParallelExecute.UseVisualStyleBackColor = true;
+            this.normalParallelExecute.Click += new System.EventHandler(this.normalParallelExecute_Click);
+            // 
+            // asyncExecute
+            // 
+            this.asyncExecute.Location = new System.Drawing.Point(12, 135);
+            this.asyncExecute.Name = "asyncExecute";
+            this.asyncExecute.Size = new System.Drawing.Size(651, 51);
+            this.asyncExecute.TabIndex = 2;
+            this.asyncExecute.Text = "Async Execute";
+            this.asyncExecute.UseVisualStyleBackColor = true;
+            this.asyncExecute.Click += new System.EventHandler(this.asyncExecute_Click);
+            // 
+            // parallelAsyncExecute
+            // 
+            this.parallelAsyncExecute.Location = new System.Drawing.Point(12, 192);
+            this.parallelAsyncExecute.Name = "parallelAsyncExecute";
+            this.parallelAsyncExecute.Size = new System.Drawing.Size(651, 51);
+            this.parallelAsyncExecute.TabIndex = 3;
+            this.parallelAsyncExecute.Text = "Parallel Async Execute";
+            this.parallelAsyncExecute.UseVisualStyleBackColor = true;
+            // 
+            // cancelation
+            // 
+            this.cancelation.Location = new System.Drawing.Point(12, 249);
+            this.cancelation.Name = "cancelation";
+            this.cancelation.Size = new System.Drawing.Size(651, 51);
+            this.cancelation.TabIndex = 4;
+            this.cancelation.Text = "Cancel Operation";
+            this.cancelation.UseVisualStyleBackColor = true;
+            // 
+            // resultBox
+            // 
+            this.resultBox.Location = new System.Drawing.Point(12, 349);
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(651, 285);
+            this.resultBox.TabIndex = 5;
+            this.resultBox.Text = "";
+            // 
+            // AsyncExample
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 646);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.cancelation);
+            this.Controls.Add(this.parallelAsyncExecute);
+            this.Controls.Add(this.asyncExecute);
+            this.Controls.Add(this.normalParallelExecute);
+            this.Controls.Add(this.normalExecute);
+            this.Name = "AsyncExample";
+            this.Text = "Async Example";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button normalExecute;
+        private System.Windows.Forms.Button normalParallelExecute;
+        private System.Windows.Forms.Button asyncExecute;
+        private System.Windows.Forms.Button parallelAsyncExecute;
+        private System.Windows.Forms.Button cancelation;
+        private System.Windows.Forms.RichTextBox resultBox;
+    }
+}
+
---
+++ WebsiteDownloader/WebsiteDownloader/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebsiteDownloader
+{
+    public partial class AsyncExample : Form
+    {
+
+
+        public AsyncExample()
+        {
+            InitializeComponent();
+
+        }
+
+        public static List<string> websites = new List<string>()
+        {
+            "https://www.google.com",
+            "https://www.microsoft.com",
+            "https://www.cnn.com",
+            "https://www.amazon.com",
+            "https://www.facebook.com",
+            "https://www.twitter.com",
+            "https://www.codeproject.com",
+            "https://www.stackoverflow.com",
+            "https://en.wikipedia.org/wiki/.NET_Framework",
+            "https://nakov.com",
+            "https://elmah.io",
+            "https://www.pluralsight.com",
+            "https://www.udemy.com"
+        };
+
+        public class WebsiteDataModel
+        {
+            public string WebsiteUrl { get; set; }
+            public string WebsiteData { get; set; }
+        }
+
+        private static WebsiteDataModel DownloadWebsite(string websiteUrl)
+        {
+            WebsiteDataModel result = new WebsiteDataModel();
+            result.WebsiteUrl = websiteUrl;
+
+            WebClient webClient = new WebClient();
+            result.WebsiteData = webClient.DownloadString(websiteUrl);
+            return result;
+        }
+
+        private async static Task<WebsiteDataModel> DownloadWebsiteAsync(string websiteUrl)
+        {
+            return await Task.Factory.StartNew(() =>
+            {
+                WebsiteDataModel result = new WebsiteDataModel();
+                result.WebsiteUrl = websiteUrl;
+
+                WebClient webClient = new WebClient();
+                result.WebsiteData = webClient.DownloadString(websiteUrl);
+                return result;
+            });
+        }
+
+        private void normalExecute_Click(object sender, EventArgs e)
+        {
+            Stopwatch sw = new Stopwatch();
+            resultBox.Clear();
+            sw.Start();
+            foreach (var site in websites)
+            {
+                resultBox.AppendText($"{site} downloaded: {DownloadWebsite(site).WebsiteData.Length} characters long\n");
+            }
+            sw.Stop();
+            resultBox.AppendText($"Total execution time: {sw.ElapsedMilliseconds}ms");
+        }
+
+        private void normalParallelExecute_Click(object sender, EventArgs e)
+        {
+            Stopwatch sw = new Stopwatch();
+            resultBox.Clear();
+            sw.Start();
+            Parallel.ForEach(websites, website =>
+            {
+                resultBox.AppendText($"{website} downloaded: {DownloadWebsite(website).WebsiteData.Length} characters long\n");
+            });
+            sw.Stop();
+            resultBox.AppendText($"Total execution time: {sw.ElapsedMilliseconds}ms");
+        }
+
+        private void asyncExecute_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
